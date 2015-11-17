cead09b93b4b47b21c4da6568af3d03a1819fc8e
ruslan-ohitin <ruslan.ohitin@gmail.com>
2015-11-17 17:58:15 +0600

    Add comments to sms_modem.sh and sendsms

--- sms_modem.sh
+++ sms_modem.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# sms_modem.sh - Zabbix alert script for sending text sms using smstools3 package
+#
+# Ruslan Ohitin 2015
+# ruslan.ohitin@gmail.com
+# https://github.com/ruslan-ohitin/zabbix-alerts
+
+# you need sendsms script somethere in your path
+
 to=$1
 subject="$2"
 body="$3"
