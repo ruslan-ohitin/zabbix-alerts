f1f7830a18151f840889f51b1aa67c7b1b307f80
ruslan-ohitin <ruslan.ohitin@gmail.com>
2015-11-17 17:22:08 +0600

    Add comments

--- send_email.sh
+++ send_email.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-MAIL_SERVER=smtp.yandex.ru
+# send_email.sh - Zabbix alert script for sending emails using msmtp package
+#
+# Ruslan Ohitin 2015
+# ruslan.ohitin@gmail.com
+# https://github.com/ruslan-ohitin/zabbix-alerts
+
+# Change this to real credentials
+MAIL_FROM="email@server.com"
+MAIL_PASSWORD="password"
+MAIL_SERVER="smtp.server.com"
 MAIL_PORT=465
+
 MAIL_DATE=`date +"%d %b %Y %T"`
-MAIL_FROM=<mail_from>
-MAIL_PASSWORD=<password>
 
 MAIL_TO=$1
 MAIL_SUBJECT=$2
@@ -18,3 +26,4 @@ MAIL_MESSAGE="Date: ${MAIL_DATE}
 \n\n${MAIL_TEXT}"
 
 echo -e ${MAIL_MESSAGE} | /usr/bin/msmtp --host=${MAIL_SERVER} --port=${MAIL_PORT} --tls=on --tls-certcheck=off --tls-starttls=off --auth=login --user=${MAIL_FROM} --passwordeval="echo ${MAIL_PASSWORD}" -f ${MAIL_FROM} ${MAIL_TO} 
+
--- send_push.sh
+++ send_push.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# send_push.sh - Zabbix alert script for sending pushes using pushbullet.com service
+#
+# First parameter should be API_KEY or API_KEY:DEV_IDEN
+# See https://docs.pushbullet.com/ for docs.
+#
+# Ruslan Ohitin 2015
+# ruslan.ohitin@gmail.com
+# https://github.com/ruslan-ohitin/zabbix-alerts
+
 param=${1}
 API_KEY=${param%:*}
 DEV_IDEN=${param#*:}
+
+TITLE_PREFIX="Prefix: " # Use this to customize pushes from different servers
 
 if [ $DEV_IDEN == $API_KEY ]; then
   DEV_CMD=""
@@ -15,5 +26,5 @@ curl https://api.pushbullet.com/api/pushes \
       ${DEV_CMD} \
       -d type=note \
       -X POST \
-      -d title="SibTorg: ${2}" \
+      -d title="${TITLE_PREFIX}${2}" \
       -d body="${3}"
